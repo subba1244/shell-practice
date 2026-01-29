bb7dc35f8d2222eb6f2d9be09a55f84383a932fb
subbareddy <subba@LAPTOP-D8U12TKH.com>
2026-01-29 09:11:28 +0530

    variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1=ramesh
-PERSON2=suresh
+PERSON1=$1
+PERSON2=$2
 echo "$PERSON1: hai $PERSON2 how are you"
 echo "$PERSON2: hello $PERSON1,i am fine.how are you"
 echo "$PERSON1: what are you learning recently"
