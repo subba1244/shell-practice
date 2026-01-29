baf62ba1e2ea3eaacf16c8e12cf589457dbf2c3d
subbareddy <subba@LAPTOP-D8U12TKH.com>
2026-01-29 06:59:50 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: hai suresh how are you"
+echo "suresh: hello ramesh,i am fine.how are you"
+echo "ramesh: what are you learning recently"
+echo suresh: I am learning shell scripting"
