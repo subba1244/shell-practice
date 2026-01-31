75ea7140ea6d21b024ddfbfc7227edf34ad4dd11
subbareddy <subba@LAPTOP-D8U12TKH.com>
2026-01-31 16:56:04 +0530

    conditions

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -3,12 +3,14 @@
 NUMBER=$(id -u)
 if [ $NUMBER -ne 0 ]; then
     echo "ERROR ::This Should be Run in as Administartor Privileges only"
+    exit 1
 
 fi
 
 dnf install mysql -y
-if [ $1 -ne 0 ]; then
+if [ $? -ne 0 ]; then
     echo "ERROR:: Installing $2 is failure"
+    exit 1
 else
     echo "Installing $2 is SUCCESS"
 
